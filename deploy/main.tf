fd79fd2ef3e49866a224b695b7b7be6937075836
Jude <jaar26@gmail.com>
2022-07-09 18:05:50 -0500

    adding tf files and bastion mods

--- deploy/main.tf
+++ deploy/main.tf
@@ -1,14 +1,18 @@
 terraform {
-    backend "s3" {
-        bucket         = "rearc-terraform-state-buckit"
-        key            = "rearc-app.tfstate"
-        region         = "us-east-1"
-        encrypt        = true
-        dynamodb_table = "recipe-app-api-devops-tf-state-lock"
-    }
+  backend "s3" {
+    bucket         = "rearc-terraform-state-buckit"
+    key            = "rearc-app.tfstate"
+    region         = "us-east-1"
+    encrypt        = true
+    dynamodb_table = "rearc-terraform-state-dynamo-db"
+  }
 }
 
 provider "aws" {
-  region = "us-east-1"
+  region  = "us-east-1"
   version = "~> 2.50.0"
+}
+
+locals {
+  prefix = "${var.prefix}-${terraform.workspace}"
 }
